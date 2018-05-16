7335808a6cda42515035cf679c3df67888ddcdec
Yuanjianxin <1814888800@qq.com>
2018-05-16 17:24:37 +0800

    debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,27 +7,25 @@ module.exports=async (ctx,next)=>{
 
     //todo verify token
 
-
-
     //todo verify para
-    if(!ctx.request.body.project || !ctx.request.body.table || !ctx.request.body.method){
+    if(!ctx.$body.project || !ctx.$body.table || !ctx.$body.method){
         ctx.error=ErrorCode.INVALID_PARAMETER;
         return ctx.status=400;
     }
 
-    if(!allowedMethod.includes(ctx.request.body.method)){
+    if(!allowedMethod.includes(ctx.$body.method)){
         ctx.error=ErrorCode.UNKNOWN_METHOD;
         return ctx.status=400;
     }
 
-    let key=ctx.request.body.project +'-'+ctx.request.body.table;
+    let key=ctx.$body.project +'-'+ctx.$body.table;
 
     if(!TableMap.has(key)){
         ctx.error=ErrorCode.INVALID_PARAMETER;
         return ctx.status=400;
     }
 
-    ctx.request.body.database=TableMap.get(key);
+    ctx.$body.database=TableMap.get(key);
 
     await next();
 
